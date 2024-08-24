76e24deb891a64241efa67a21c7ba367ab888e32
Monjure Alam <monjur00@gmail.com>
2024-08-24 21:51:52 +0530

    feat: maybe monad added

--- monads.go
+++ monads.go
@@ -1,15 +1,27 @@
 package gofp
 
-type Monad[T any] interface {
-	Bind(BindFn[T]) Monad[T]
-}
+type (
+	Monad[T any] interface {
+		Bind(BindFn[T]) Monad[T]
+	}
 
-type BindFn[T any] func(T) Monad[T]
+	BindFn[T any] func(T) Monad[T]
 
-type Try[T any] struct {
-	Result T
-	Err    error
-}
+	Try[T any] struct {
+		Result T
+		Err    error
+	}
+
+	// TODO: Either[R, L any] for more generic options
+	Either[R any] struct {
+		Right R
+		Left  error
+	}
+
+	Maybe[T comparable] struct {
+		Value T
+	}
+)
 
 func (t Try[T]) Bind(b BindFn[T]) Monad[T] {
 	if t.Err != nil {
@@ -23,12 +35,6 @@ func NewTry[T any](t T, err error) Try[T] {
 	return Try[T]{t, err}
 }
 
-// TODO: Either[R, L any] for more generic options
-type Either[R any] struct {
-	Right R
-	Left  error
-}
-
 func NewEither[R any](r R, l error) Either[R] {
 	return Either[R]{r, l}
 }
@@ -39,4 +45,17 @@ func (e Either[R]) Bind(b BindFn[R]) Monad[R] {
 	}
 
 	return b(e.Right)
+}
+
+func NewMaybe[T comparable](t T) Maybe[T] {
+	return Maybe[T]{t}
+}
+
+func (m Maybe[T]) Bind(b BindFn[T]) Monad[T] {
+	var t T
+	if m.Value == t {
+		return &Maybe[T]{m.Value}
+	}
+
+	return b(m.Value)
 }
