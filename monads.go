f2abd6379972533bc605560cbe916b3905ae068f
Monjure Alam <monjur00@gmail.com>
2024-08-24 21:42:11 +0530

    feat: either monad added

--- monads.go
+++ monads.go
@@ -11,7 +11,7 @@ type Try[T any] struct {
 	Err    error
 }
 
-func (t *Try[T]) Bind(b BindFn[T]) Monad[T] {
+func (t Try[T]) Bind(b BindFn[T]) Monad[T] {
 	if t.Err != nil {
 		return &Try[T]{t.Result, t.Err}
 	}
@@ -21,4 +21,22 @@ func (t *Try[T]) Bind(b BindFn[T]) Monad[T] {
 
 func NewTry[T any](t T, err error) Try[T] {
 	return Try[T]{t, err}
+}
+
+// TODO: Either[R, L any] for more generic options
+type Either[R any] struct {
+	Right R
+	Left  error
+}
+
+func NewEither[R any](r R, l error) Either[R] {
+	return Either[R]{r, l}
+}
+
+func (e Either[R]) Bind(b BindFn[R]) Monad[R] {
+	if e.Left != nil {
+		return &Either[R]{e.Right, e.Left}
+	}
+
+	return b(e.Right)
 }
